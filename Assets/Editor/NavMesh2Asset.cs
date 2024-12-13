44cac00fa22e402deaf8a97807e23493e8e0d094
pioj <piojete@gmail.com>
2024-12-13 12:37:02 +0100

    1st Commit : Migrated
    Moved from my old repository

---
+++ Assets/Editor/NavMesh2Asset.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+public class NavMesh2Asset : MonoBehaviour
+{
+    [MenuItem("GamaScorpio/Create Mesh from NavMesh")]
+    static void ExportNavMesh()
+    {
+        NavMeshTriangulation triangulatedNavMesh = NavMesh.CalculateTriangulation();
+ 
+        var clone = new GameObject("BakedNav01", typeof(MeshFilter));
+        var clonecomp = clone.GetComponent<MeshFilter>();
+        
+        clonecomp.sharedMesh = new Mesh();
+        clonecomp.sharedMesh.name = "exported";
+        clonecomp.sharedMesh.vertices = triangulatedNavMesh.vertices;
+        clonecomp.sharedMesh.triangles = triangulatedNavMesh.indices;
+        
+        clonecomp.sharedMesh.RecalculateBounds();
+        clonecomp.sharedMesh.RecalculateNormals();
+        clonecomp.sharedMesh.RecalculateTangents();
+        MeshUtility.Optimize(clonecomp.sharedMesh);
+
+        clone.AddComponent<MeshRenderer>();
+        var mat = new Material(Shader.Find("Standard"));
+        clone.GetComponent<MeshRenderer>().sharedMaterial = mat;
+        
+        SaveMesh(clonecomp.sharedMesh, clonecomp.sharedMesh.name, true);
+        
+    }
+
+    public static void SaveMesh (Mesh mesh, string name, bool makeNewInstance) {
+        string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
+        if (string.IsNullOrEmpty(path)) return;
+        
+        path = FileUtil.GetProjectRelativePath(path);
+
+        Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
+		
+        AssetDatabase.CreateAsset(meshToSave, path);
+        AssetDatabase.SaveAssets();
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# NavMesh2Asset
+ Simple tool that generates a mesh Asset from a baked NavMesh.
+
+### Author
+[☕ Carlos Lecina](https://ko-fi.com/carloslecina)
+
+### Overview
+ Sometimes you need a lowpoly mesh from your level for testing purposes, tweaking level design and/or navigation, etc.
+ This little script solves this problem by dumping your current baked Navigation Mesh into a proper Mesh GameObject in your Scene.
+ Also, it prompts you to save the mesh as a 3D asset into your Project.
+  
+### Instructions
+ 1. Select your GameObjects and bake your preferred Navigation Mesh.
+ 2. Run the tool from the top menu bar.
+ 
+### Future Upgrades
+ - Adding PROBuilder to this project would let you extrude the baked mesh, effectively creating a proper (editable in PROBUILDER) low polygon level aka *"GrayBoxing"*
